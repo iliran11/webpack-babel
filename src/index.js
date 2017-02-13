bdb688ac1ada323fc7bff2f9715416c7e71fe099
iliran11 <iliran11@gmail.com>
2017-02-13 02:49:01 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {sayHello} from './moudle';
+const element = document.createElement('h1');
+element.innerHTML = sayHello('World');
+document.body.appendChild(element);
+const liran = "hello";
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: 'dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({title: 'Tree-shaking'})]
+};
